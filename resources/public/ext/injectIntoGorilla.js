7fa80d6a3c3855a2a95c04aa3347b0c5e9524221
Marcus Lewis <mrcslws@gmail.com>
2015-05-28 10:38:29 -0400

    Hacky extension point. Also, accidental whitespace changes. DO NOT MERGE.

---
+++ resources/public/ext/injectIntoGorilla.js
@@ -0,0 +1,4 @@
+/* Supply your own ext/injectIntoGorilla.js to override this blank file.
+   e.g. use
+       :resource-paths ["resources" "target/resources"]
+   and create file resources/public/ext/injectIntoGorilla.js */
