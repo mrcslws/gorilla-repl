b2801d6111f5b73421e04c91523b78c2dcf02a9a
Marcus Lewis <mrcslws@gmail.com>
2015-06-02 09:15:51 -0700

    More hacks, accidental whitespace changes

--- resources/public/js-viewer/main-viewer.js
+++ resources/public/js-viewer/main-viewer.js
@@ -11,8 +11,8 @@ var app = function () {
     self.worksheet = ko.observable();
     self.filename = ko.observable("");
     self.title = ko.computed(function () {
-        if (self.filename() === "") return "Gorilla REPL viewer";
-        else return "Gorilla REPL viewer: " + self.filename();
+	if (self.filename() === "") return "Gorilla REPL viewer";
+	else return "Gorilla REPL viewer: " + self.filename();
     });
     self.sourceURL = ko.observable("");
     self.source = ko.observable("");
@@ -21,27 +21,27 @@ var app = function () {
     // The copyBox is a UI element that gives links to the source of the worksheet, and how to copy/edit it.
     self.copyBoxVisible = ko.observable(false);
     self.showCopyBox = function () {
-        self.copyBoxVisible(true);
+	self.copyBoxVisible(true);
     };
     self.hideCopyBox = function () {
-        self.copyBoxVisible(false);
+	self.copyBoxVisible(false);
     };
 
     self.start = function (worksheetData, sourceURL, worksheetName, source) {
 
-        var ws = worksheet();
-        ws.segments = ko.observableArray(worksheetParser.parse(worksheetData));
-        self.worksheet(ws);
-        self.sourceURL(sourceURL);
-        self.filename(worksheetName);
-        self.source(source);
-        self.host((source.toLowerCase() === "bitbucket") ? "Bitbucket" : "GitHub");
+	var ws = worksheet();
+	ws.segments = ko.observableArray(worksheetParser.parse(worksheetData));
+	self.worksheet(ws);
+	self.sourceURL(sourceURL);
+	self.filename(worksheetName);
+	self.source(source);
+	self.host((source.toLowerCase() === "bitbucket") ? "Bitbucket" : "GitHub");
 
-        // wire up the UI
-        ko.applyBindings(self, document.getElementById("document"));
+	// wire up the UI
+	ko.applyBindings(self, document.getElementById("document"));
 
-        // we only use CodeMirror to syntax highlight the code in the viewer
-        CodeMirror.colorize($("pre.static-code"), "text/x-clojure");
+	// we only use CodeMirror to syntax highlight the code in the viewer
+	CodeMirror.colorize($("pre.static-code"), "text/x-clojure");
 
     };
 
@@ -59,34 +59,39 @@ $(function () {
     // how are we getting the worksheet data?
     var source = getParameterByName("source");
     switch (source) {
-        case "github":
-            var user = getParameterByName("user");
-            var repo = getParameterByName("repo");
-            var path = getParameterByName("path");
-            getFromGithub(user, repo, path, function (data) {
-                viewer.start(data, "https://github.com/" + user + "/" + repo, path, source);
-            });
-            return;
-        case "gist":
-            var id = getParameterByName("id");
-            var filename = getParameterByName("filename");
-            getFromGist(id, filename, function (data) {
-                viewer.start(data,  "https://gist.github.com/" + id, filename, source);
-            });
-            return;
-        case "bitbucket":
-            var user = getParameterByName("user");
-            var repo = getParameterByName("repo");
-            var path = getParameterByName("path");
-            var revision = getParameterByName("revision") || "HEAD";
-            getFromBitbucket(user, repo, path, revision, function (data) {
-                viewer.start(data, "https://bitbucket.org/" + user + "/" + repo, path, source);
-            });
-            return;
-        case "test":
-            // so you can test without exhausting the github API limit
-            $.get('/test.clj').success(function (data) {
-                viewer.start(data, "http://gorilla-repl.org/", "test.clj", source);
-            });
+	case "github":
+	    var user = getParameterByName("user");
+	    var repo = getParameterByName("repo");
+	    var path = getParameterByName("path");
+	    getFromGithub(user, repo, path, function (data) {
+		viewer.start(data, "https://github.com/" + user + "/" + repo, path, source);
+	    });
+	    return;
+	case "gist":
+	    var id = getParameterByName("id");
+	    var filename = getParameterByName("filename");
+	    getFromGist(id, filename, function (data) {
+		viewer.start(data,  "https://gist.github.com/" + id, filename, source);
+	    });
+	    return;
+	case "bitbucket":
+	    var user = getParameterByName("user");
+	    var repo = getParameterByName("repo");
+	    var path = getParameterByName("path");
+	    var revision = getParameterByName("revision") || "HEAD";
+	    getFromBitbucket(user, repo, path, revision, function (data) {
+		viewer.start(data, "https://bitbucket.org/" + user + "/" + repo, path, source);
+	    });
+	    return;
+	case "test":
+	    // so you can test without exhausting the github API limit
+	    $.get('/test.clj').success(function (data) {
+		viewer.start(data, "http://gorilla-repl.org/", "test.clj", source);
+	    });
+    case "url":
+	var path = getParameterByName("path");
+	$.get(path).success(function (data) {
+	    viewer.start(data, "http://fake-website.org/", "fake-filename.clj", source);
+	});
     }
 });
